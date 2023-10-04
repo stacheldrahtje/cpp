f9ffbf6c16cd665c05b908e4fff1ddb1c7d955e2
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-10-04 15:49:16 +0200

    Update adduser.h

--- radagast-related/adduser/adduser.h
+++ radagast-related/adduser/adduser.h
@@ -18,7 +18,7 @@ int fnc_adduser (char[]);
 int Adduser::fnc_adduser (char user[])
 {
 uname = user;
-exit_code = WEXITSTATUS(system(("sudo useradd -c <fullname>"+uname).c_str()));
+exit_code = WEXITSTATUS(system(("sudo useradd -c <fullname>"+uname).c_str())); //idea: combine char[] for full name and username to one string with a space in between.
 cout << "The exitcode is: " << exit_code << endl;
 return exit_code;
 };
