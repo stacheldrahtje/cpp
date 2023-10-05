8e6c55c7355c4338a82715e2d640c5a1a0acedac
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-10-05 07:43:54 +0200

    anonimized code

--- radagast-related/adduser/combi.cpp
+++ radagast-related/adduser/combi.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    char username[16]="nament";
-    char fullname[16]="Natasha";
+    char username[16]="<username>";
+    char fullname[16]="<fullname>";
     string combi = username;
     combi += " -c ";
     combi += fullname;
