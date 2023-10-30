0f6b8effe3a0ce03b061d9347aa278c3f4ca6b20
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-10-30 13:46:44 +0100

    Update command.cpp

--- radagast-related/system/command.cpp
+++ radagast-related/system/command.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int adduser(string uname, string fname)
 {
-  int returncode;
+  int exit_code;
   exit_code = WEXITSTATUS(system("sudo whoami"));  
-
+  return exit_code;
 }
 
 int setpasswd(string passwd)
